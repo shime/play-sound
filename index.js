a6157dedb04e46316f8c4ab8c1fa7b62d03287db
Hrvoje Šimić <shime@twobucks.co>
2017-12-13 17:22:16 +0100

    fix: whitespace and semicolons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs               = require('fs')
   , findExec         = require('find-exec')
-  , spawn    = require('child_process').spawn
+  , spawn            = require('child_process').spawn
   , players          = [
                         'mplayer',
                         'afplay',
@@ -21,27 +21,27 @@ function Play(opts){
   // Regex by @stephenhay from https://mathiasbynens.be/demo/url-regex
 
   this.play = function(what, options, next){
-    next  = next || function(){};
-    next  = typeof options === 'function' ? options : next;
-    options = typeof options === 'object' ? options : {};
-	options.stdio = 'ignore';
+    next  = next || function(){}
+    next  = typeof options === 'function' ? options : next
+    options = typeof options === 'object' ? options : {}
+    options.stdio = 'ignore'
 
-    var isURL = this.player == 'mplayer' && this.urlRegex.test(what);
+    var isURL = this.player == 'mplayer' && this.urlRegex.test(what)
 
-    if (!what) return next(new Error("No audio file specified"));
+    if (!what) return next(new Error("No audio file specified"))
 
     if (!this.player){
       return next(new Error("Couldn't find a suitable audio player"))
     }
 
     var args = Array.isArray(options[this.player]) ? options[this.player].concat(what) : [what]
-	var process = spawn(this.player, args, options);
-	if (!process) {
-		next(new Error("unable to spawn process with "+this.player));
-		return null;
-	}
-	process.on('close',function(err){ next(err && !err.killed ? err : null); });
-	return process;
+    var process = spawn(this.player, args, options)
+    if (!process) {
+      next(new Error("Unable to spawn process with " + this.player))
+      return null
+    }
+    process.on('close',function(err){ next(err && !err.killed ? err : null) })
+    return process
   }
 
   this.test = function(next) { this.play('./assets/test.mp3', next) }
